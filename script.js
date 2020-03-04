b7a87db9b944d8735d26e6b2aa034ea028911d8a
JidMenosta <jidmenosta@gmail.com>
2020-03-04 12:57:26 -0500

    changed some of the logic in the code

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+let alphaSmall = "";
+let alphaLarge = generateBig();
+let numbers = generateNumbers();
+let special = generateSpecial();
 
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
+    // let alphaSmall = generateSmall();
+    // let alphaLarge = generateBig();
+    // let numbers = generateNumbers();
+    // let special = generateSpecial();
 
     passwordText.value = password;
 
@@ -44,9 +52,54 @@ function generatePassword() {
     } else {
         alert("Got it");
     }
+    console.log(passLength + " " + alphaSmall + " " + alphaLarge + " " + numbers + " " + special);
+
 
 }
-console.log(passLength + " " + alphaSmall + " " + alphaLarge + " " + numbers + " " + special);
+
+function generateSmall(passLength) {
+    let alphaSmall = "";
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    for (i = 0; i < passLength; i++) {
+        alphaSmall += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    }
+    console.log(alphaSmall);
+    return (alphaSmall);
+}
+
+function generateBig(passLength) {
+    let results = "";
+    let alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for (i = 0; i < passLength; i++) {
+        results += alphabetUpper.charAt(Math.floor(Math.random() * alphabetUpper.length));
+    }
+    console.log(results);
+    return (results);
+}
+
+function generateNumbers(passLength) {
+    let results = "";
+    let number = "0123456789";
+
+    for (i = 0; i < passLength; i++) {
+        results += number.charAt(Math.floor(Math.random() * number.length));
+    }
+    console.log(results);
+    return (results);
+}
+
+function generateSpecial(passLength) {
+    let results = "";
+    let special = "!@#$%^&*(){}[];:~";
+
+    for (i = 0; i < passLength; i++) {
+        results += special.charAt(Math.floor(Math.random() * special.length));
+    }
+    console.log(results);
+    return (results);
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
