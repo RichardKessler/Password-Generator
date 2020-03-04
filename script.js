92c3df16d69fbc74101d893f26ae9ee5dfe039ac
JidMenosta <jidmenosta@gmail.com>
2020-03-04 17:59:14 -0500

    Made a lot of changes to the logic of the program

--- script.js
+++ script.js
@@ -1,105 +1,71 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let alphaSmall = "";
-let alphaLarge = generateBig();
-let numbers = generateNumbers();
-let special = generateSpecial();
+let alphaSmall = "abcdefghijklmnopqrstuvwxyz";
+let caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "0123456789";
+let special = "!@#$%^&*()_+-=[]{}|\?><,/;':";
+
 
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-    // let alphaSmall = generateSmall();
-    // let alphaLarge = generateBig();
-    // let numbers = generateNumbers();
-    // let special = generateSpecial();
 
+    console.log(password);
     passwordText.value = password;
-
 }
 
 function generatePassword() {
-    let passLength = prompt("How long do you want your password to be?  Please choose 8-128");
-    if (passLength < 8 || passLength > 128) {
-        alert("Please input a number from 8 to 128.");
-        prompt("How long do you want your password to be?");
+    // make a loop for the logic
+    let strength = prompt("How strong would you like your password to be? Must choose between 8-128 characters.");
+    console.log(strength);
+    if (strength < 8 && strength > 128) {
+        alert("Please input a number from 8 - 128.")
+        prompt("How strong do you want your password?");
+    }
+    let alpha = confirm("Do you want lowercase letters?");
+    let alphaResult = "";
+
+    let upper = confirm("Do you want uppercase letters?");
+    let upperResult = "";
+
+    let int = confirm("Do you want to include numbers?")
+    let intResult = "";
+
+    let char = confirm("Do you want special characters?");
+    let charResult = "";
+
+    for (let i = 0; i < parseInt(strength); i++) {
+
+        if (alpha) {
+            alphaResult += alphaSmall.charAt(Math.floor(Math.random() * alphaSmall.length + strength));
+        }
+        console.log(alphaResult);
+
+        if (upper) {
+            upperResult += special.charAt(Math.floor(Math.random() * special.length + strength));
+        }
+        console.log(upperResult);
+
+        if (int) {
+            intResult += numbers.charAt(Math.floor(Math.random() * numbers.length + strength));
+        }
+        console.log(intResult);
+
+        if (char) {
+            charResult += special.charAt(Math.floor(Math.random() * special.length));
+        }
+        console.log(charResult);
+
+
     }
 
-    let alphaSmall = confirm("Do you want lowercase letters?");
-    if (alphaSmall) {
-        alert("Thank you");
-    } else {
-        alert("Got it");
-    }
 
-    let alphaLarge = confirm("Do you want uppercase letters?");
-    if (alphaLarge) {
-        alert("Thank you");
-    } else {
-        alert("Got it");
-    }
-
-    let numbers = confirm("Do you want numbers?");
-    if (numbers) {
-        alert("Thank you");
-    } else {
-        alert("Got it");
-    }
-
-    let special = confirm("Do you want special characters?");
-    if (special) {
-        alert("Thank you");
-    } else {
-        alert("Got it");
-    }
-    console.log(passLength + " " + alphaSmall + " " + alphaLarge + " " + numbers + " " + special);
-
-
+    return alphaResult, upperResult, intResult, charResult;
 }
 
-function generateSmall(passLength) {
-    let alphaSmall = "";
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    for (i = 0; i < passLength; i++) {
-        alphaSmall += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-    }
-    console.log(alphaSmall);
-    return (alphaSmall);
-}
 
-function generateBig(passLength) {
-    let results = "";
-    let alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    for (i = 0; i < passLength; i++) {
-        results += alphabetUpper.charAt(Math.floor(Math.random() * alphabetUpper.length));
-    }
-    console.log(results);
-    return (results);
-}
-
-function generateNumbers(passLength) {
-    let results = "";
-    let number = "0123456789";
-
-    for (i = 0; i < passLength; i++) {
-        results += number.charAt(Math.floor(Math.random() * number.length));
-    }
-    console.log(results);
-    return (results);
-}
-
-function generateSpecial(passLength) {
-    let results = "";
-    let special = "!@#$%^&*(){}[];:~";
-
-    for (i = 0; i < passLength; i++) {
-        results += special.charAt(Math.floor(Math.random() * special.length));
-    }
-    console.log(results);
-    return (results);
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
