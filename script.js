126f582e276e4a7218870da67b1c7d8c838390a8
JidMenosta <jidmenosta@gmail.com>
2020-03-05 19:38:59 -0500

    fixed the logic to run properly. need to get the else statement to loop back and get a proper input

--- script.js
+++ script.js
@@ -17,57 +17,113 @@ function writePassword() {
 
 function generatePassword() {
     // make a loop for the logic
-    let strength = prompt("How strong would you like your password to be? Must choose between 8-128 characters.");
+    let strength = prompt("How strong would you like your password to be? Must choose between 8-128 characters.  Must chose at least 1 option.");
     console.log(strength);
-    if (strength < 8 && strength > 128) {
-        alert("Please input a number from 8 - 128.")
-        prompt("How strong do you want your password?");
-    }
-    let alpha = confirm("Do you want lowercase letters?");
-    let alphaResult = "";
+    if (strength > 8 && strength < 128) {
+        let alpha = confirm("Do you want lowercase letters?");
+        let alphaResult = "";
 
-    let upper = confirm("Do you want uppercase letters?");
-    let upperResult = "";
+        let upper = confirm("Do you want uppercase letters?");
+        let upperResult = "";
 
-    let int = confirm("Do you want to include numbers?")
-    let intResult = "";
+        let int = confirm("Do you want to include numbers?")
+        let intResult = "";
 
-    let char = confirm("Do you want special characters?");
-    let charResult = "";
+        let char = confirm("Do you want special characters?");
+        let charResult = "";
 
+        for (let i = 0; i < parseInt(strength); i++) {
 
+            if (alpha) {
+                alphaResult += alphaSmall.charAt(Math.floor(Math.random() * alphaSmall.length));
+            }
+            // console.log(alphaResult);
 
-    for (let i = 0; i < parseInt(strength); i++) {
+            if (upper) {
+                upperResult += caps.charAt(Math.floor(Math.random() * caps.length));
+            }
+            // console.log(upperResult);
 
-        if (alpha) {
-            alphaResult += alphaSmall.charAt(Math.floor(Math.random() * alphaSmall.length));
+            if (int) {
+                intResult += numbers.charAt(Math.floor(Math.random() * numbers.length));
+            }
+            // console.log(intResult);
+
+            if (char) {
+                charResult += special.charAt(Math.floor(Math.random() * special.length));
+            }
+
         }
-        console.log(alphaResult);
 
-        if (upper) {
-            upperResult += caps.charAt(Math.floor(Math.random() * caps.length));
-        }
-        console.log(upperResult);
-
-        if (int) {
-            intResult += numbers.charAt(Math.floor(Math.random() * numbers.length));
-        }
-        console.log(intResult);
-
-        if (char) {
-            charResult += special.charAt(Math.floor(Math.random() * special.length));
-        }
-        console.log(charResult);
+        console.log(alphaResult, upperResult, intResult, charResult);
         let draftPassword = alphaResult + upperResult + intResult + charResult;
         let finalPassword = draftPassword.split('').sort(function() { return 0.5 - Math.random() }).join('');
+        let userPassword = finalPassword.substring(0, strength);
         console.log(draftPassword);
         console.log(finalPassword);
-        return finalPassword;
+
+        return userPassword;
+        // return alphaResult, upperResult, intResult, charResult, finalPassword;
+        // return finalPassword;
+    } else {
+        alert("Please input a number from 8 - 128.");
+        let strength = prompt("How strong do you want your password?");
+        console.log(strength);
     }
+    // let alpha = confirm("Do you want lowercase letters?");
+    // let alphaResult = "";
+
+    // let upper = confirm("Do you want uppercase letters?");
+    // let upperResult = "";
+
+    // let int = confirm("Do you want to include numbers?")
+    // let intResult = "";
+
+    // let char = confirm("Do you want special characters?");
+    // let charResult = "";
+
+    // let finalPassword = "";
 
 
-    // return alphaResult, upperResult, intResult, charResult, finalPassword;
-    // return finalPassword;
+
+    // for (let i = 0; i < parseInt(strength); i++) {
+
+    //     if (alpha) {
+    //         alphaResult += alphaSmall.charAt(Math.floor(Math.random() * alphaSmall.length));
+    //     }
+    //     // console.log(alphaResult);
+
+    //     if (upper) {
+    //         upperResult += caps.charAt(Math.floor(Math.random() * caps.length));
+    //     }
+    //     // console.log(upperResult);
+
+    //     if (int) {
+    //         intResult += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    //     }
+    //     // console.log(intResult);
+
+    //     if (char) {
+    //         charResult += special.charAt(Math.floor(Math.random() * special.length));
+    //     }
+    //     // console.log(charResult);
+    //     // let draftPassword = alphaResult + upperResult + intResult + charResult;
+    //     // let finalPassword = draftPassword.split('').sort(function() { return 0.5 - Math.random() }).join('');
+    //     // console.log(draftPassword);
+    //     // console.log(finalPassword);
+    //     // return finalPassword;
+    // }
+
+    // console.log(alphaResult, upperResult, intResult, charResult);
+    // let draftPassword = alphaResult + upperResult + intResult + charResult;
+    // let finalPassword = draftPassword.split('').sort(function() { return 0.5 - Math.random() }).join('');
+    // let userPassword = finalPassword.substring(0, strength);
+    // console.log(draftPassword);
+    // console.log(finalPassword);
+
+    // return userPassword;
+    // // return alphaResult, upperResult, intResult, charResult, finalPassword;
+    // // return finalPassword;
 }
 
 
